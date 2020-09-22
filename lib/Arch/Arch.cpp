5bc471461d4ae97269a8e6fa914842ea3f920972
Peter Goodman <peter.goodman@gmail.com>
2020-09-22 16:05:25 -0400

    Fixes for aarch64

--- lib/Arch/Arch.cpp
+++ lib/Arch/Arch.cpp
@@ -81,8 +81,10 @@ CallingConvention::CreateCCFromArch(const remill::Arch *arch) {
         break;
       }
 
-    // Fallthrough for unsupported architectures
     case remill::kArchAArch64LittleEndian:
+      return CreateAArch64_C(arch);
+
+    // Fallthrough for unsupported architectures
     default: break;
   }
 
