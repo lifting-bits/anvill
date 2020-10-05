801e8bc3b29ec924bf27aa835b6296bc4706ce62
Artem Dinaburg <artem@trailofbits.com>
2020-10-05 12:53:26 -0400

    More logging for test runners

--- ci/install_binja.sh
+++ ci/install_binja.sh
@@ -4,14 +4,23 @@ set -euo pipefail
 
 # Decrypt any CI secrets
 function decrypt {
-    gpg --quiet --batch --yes --decrypt \
-        --passphrase="${BINJA_DECODE_KEY}" \
-        --output ${1} ${1}.gpg 
+    if [[ -f ${1}.gpg ]]
+    then
+        echo "Decrypting file: ${1}.gpg"
+        gpg --quiet --batch --yes --decrypt \
+            --passphrase="${BINJA_DECODE_KEY}" \
+            --output "${1}" "${1}.gpg" 
+    else
+        echo "Could not find file: ${1}.gpg"
+        return 1
+    fi
 }
 
 EXTRACT_DIR=${VIRTUAL_ENV:-"/opt/vector35/binaryninja"}
 
+echo "Decrypting Binja..."
 decrypt BinaryNinja-headless.zip
+echo "Decrypting license..."
 decrypt license.txt
 
 #Run this from the bmef root directory and it will install Binja for you
@@ -19,6 +28,6 @@ unzip BinaryNinja-headless.zip -d ${EXTRACT_DIR}
 mkdir -p ~/.binaryninja/
 cp license.txt ~/.binaryninja/license.dat
 chmod +x ${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh
-${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh -s -d -m -l
+${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh -s -d -m -l &> /dev/null
 # virtual env, use -v; if not don't use it
 python3 ${EXTRACT_DIR}/binaryninja/scripts/install_api.py ${VIRTUAL_ENV+"-v"}
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
+CLANG=$(which clang)
+
+echo "Running round-trip tests using: ${CLANG}"
 python3 /opt/trailofbits/anvill/share/roundtrip.py \
   /opt/trailofbits/anvill/bin/anvill-decompile-json* \
   /opt/trailofbits/anvill/share/tests \
-  $(which clang)
+  "${CLANG}"
