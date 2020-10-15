f298ffdfe2726bb8344e4ae91f5a8f3fb88e9e76
Artem Dinaburg <artem@trailofbits.com>
2020-10-15 03:06:51 -0400

    Fix tests for anvill updates

--- ci/install_clang.sh
+++ ci/install_clang.sh
@@ -2,5 +2,26 @@
 
 set -euo pipefail
 
+V=""
+case ${LLVM_VERSION} in
+  llvm80*)
+    V=8
+  ;;
+  llvm90*)
+    V=9
+  ;;
+  llvm100*)
+    V=10
+  ;;
+  llvm110*)
+    V=11
+  ;;
+  *)
+    echo "Unknown or unsupported LLVM version: ${LLVM_VERSION}"
+    exit 1
+  ;;
+esac
+
+
 apt-get update &> /dev/null
-apt-get install -qqy clang &> /dev/null
+apt-get install -qqy clang-${V} &> /dev/null
