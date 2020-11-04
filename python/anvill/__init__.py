5157a1200ca4b451398564f32df853e34c18215a
Carson Harmon <carsonharmon@protonmail.com>
2020-11-04 08:25:36 -0800

    Remove default path check in __init__ for headless

--- python/anvill/__init__.py
+++ python/anvill/__init__.py
@@ -5,17 +5,14 @@ try:
     from .ida import *
 except ImportError as e:
     try:
-        print("Trying binja!")
         import binaryninja
-        license_file_path = "~/.binaryninja/license.dat"
         if os.getenv("BINJA_LICENSE") is not None:
             license_file_path = os.getenv("BINJA_LICENSE")
             if not os.path.exists(license_file_path):
                 raise FileNotFoundError(f"Error! Could not find license file at {license_file_path}")
-        # Load license
-        with open(license_file_path, "r") as license_file:
-            binaryninja.core_set_license(license_file.read())
-        print("Importing .binja!")
+            # Load license
+            with open(license_file_path, "r") as license_file:
+                binaryninja.core_set_license(license_file.read())
         from .binja import *
 
     except ImportError as e:
