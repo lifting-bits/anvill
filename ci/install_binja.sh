3efcc8b3e6cf785c1dfea3b0525ea104c563fd54
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-12-10 15:17:58 -0500

    Try creating directory before unzipping binja

--- ci/install_binja.sh
+++ ci/install_binja.sh
@@ -15,7 +15,7 @@ function decrypt {
         echo "Decrypting file: ${1}.gpg"
         gpg --quiet --batch --yes --decrypt \
             --passphrase="${BINJA_DECODE_KEY}" \
-            --output "${1}" "${1}.gpg" 
+            --output "${1}" "${1}.gpg"
     else
         echo "Could not find file: ${1}.gpg"
         return 1
@@ -23,6 +23,7 @@ function decrypt {
 }
 
 EXTRACT_DIR=${VIRTUAL_ENV:-"/opt/vector35/binaryninja"}
+mkdir -p "${EXTRACT_DIR}"
 
 echo "Decrypting Binja..."
 decrypt BinaryNinja-headless.zip
@@ -30,10 +31,10 @@ echo "Decrypting license..."
 decrypt license.txt
 
 #Run this from the bmef root directory and it will install Binja for you
-unzip BinaryNinja-headless.zip -d ${EXTRACT_DIR}
+unzip BinaryNinja-headless.zip -d "${EXTRACT_DIR}"
 mkdir -p ~/.binaryninja/
 cp license.txt ~/.binaryninja/license.dat
-chmod +x ${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh
-${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh -s -d -m -l &> /dev/null
+chmod +x "${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh"
+"${EXTRACT_DIR}/binaryninja/scripts/linux-setup.sh" -s -d -m -l &> /dev/null
 # virtual env, use -v; if not don't use it
-python3.8 ${EXTRACT_DIR}/binaryninja/scripts/install_api.py ${VIRTUAL_ENV+"-v"}
+python3.8 "${EXTRACT_DIR}/binaryninja/scripts/install_api.py" ${VIRTUAL_ENV+"-v"}
