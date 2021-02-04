5e73d8a5f9a661230d32961ea5c736a8465b9551
Carson Harmon <carsonharmon@protonmail.com>
2021-02-04 11:14:51 -0800

    Handle case where Binja variable type is None

--- python/anvill/binja.py
+++ python/anvill/binja.py
@@ -378,6 +378,8 @@ class BNFunction(Function):
         elif isinstance(item_or_list, mlinst):
             results.extend(self._extract_types_mlil(bv, item_or_list.operands, initial_inst))
         elif isinstance(item_or_list, bn.Variable):
+            if item_or_list.type is None:
+                return results
             # We only care about registers that represent pointers.
             if item_or_list.type.type_class == bn.TypeClass.PointerTypeClass:
                 if item_or_list.source_type == bn.VariableSourceType.RegisterVariableSourceType:
