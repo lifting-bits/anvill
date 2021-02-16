ed423883cd41e2f319c96b23bc94e209730fdade
Alessandro Gario <alessandro.gario@gmail.com>
2021-02-16 18:21:28 -0500

    CMake: Refactor

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,6 +6,10 @@
 # the LICENSE file found in the root directory of this source tree.
 #
 
+set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Build type")
+
 option(ANVILL_ENABLE_INSTALL_TARGET "Set to ON to enable the install directives. This installs both the native and python components")
 option(ANVILL_ENABLE_TESTS "Set to ON to enable the tests")
 option(ANVILL_INSTALL_PYTHON3_LIBS "Install Python 3 libraries to the **local machine** at build time. Mostly used for local development, not required for packaging")
+
+set(VCPKG_ROOT "" CACHE FILEPATH "Root directory to use for vcpkg-managed dependencies")
