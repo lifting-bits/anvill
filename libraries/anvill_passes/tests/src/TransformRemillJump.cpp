d6eb5a581085e3f575b5c8ffe967e94da6f0306a
Akshay K <iit.akshay@gmail.com>
2021-04-11 14:36:21 -0400

    add more tests

--- libraries/anvill_passes/tests/src/TransformRemillJump.cpp
+++ libraries/anvill_passes/tests/src/TransformRemillJump.cpp
@@ -29,10 +29,10 @@
 
 namespace anvill {
 
-TEST_SUITE("TransformRemillJump") {
+TEST_SUITE("TransformRemillJump_Test0") {
   TEST_CASE("Run the pass on function having _remill_jump as tail call") {
     llvm::LLVMContext llvm_context;
-    auto module = LoadTestData(llvm_context, "TransformRemillJumpData.ll");
+    auto module = LoadTestData(llvm_context, "TransformRemillJumpData0.ll");
 
     auto arch = remill::Arch::Build(&llvm_context, remill::GetOSName("linux"),
                                     remill::GetArchName("amd64"));
@@ -46,7 +46,63 @@ TEST_SUITE("TransformRemillJump") {
     CHECK(RunFunctionPass(module.get(),
                           CreateTransformRemillJumpIntrinsics(lifter)));
 
-    REQUIRE(module->getFunction("__remill_jump") == nullptr);
+    const auto ret_func = module->getFunction("__remill_function_return");
+    const auto jmp_func = module->getFunction("__remill_jump");
+
+    REQUIRE((ret_func && !ret_func->use_empty()));
+    REQUIRE((!jmp_func || jmp_func->use_empty()));
+  }
+}
+
+TEST_SUITE("TransformRemillJump_Test1") {
+  TEST_CASE("Run the pass on function having _remill_jump as tail call") {
+    llvm::LLVMContext llvm_context;
+    auto module = LoadTestData(llvm_context, "TransformRemillJumpData1.ll");
+
+    auto arch = remill::Arch::Build(&llvm_context, remill::GetOSName("linux"),
+                                    remill::GetArchName("amd64"));
+    REQUIRE(arch != nullptr);
+
+    anvill::LifterOptions options(arch.get(), *module.get(), nullptr);
+
+    // memory and types will not get used and create lifter with null
+    anvill::EntityLifter lifter(options, nullptr, nullptr);
+
+    CHECK(RunFunctionPass(module.get(),
+                          CreateTransformRemillJumpIntrinsics(lifter)));
+
+    const auto ret_func = module->getFunction("__remill_function_return");
+    const auto jmp_func = module->getFunction("__remill_jump");
+
+
+    REQUIRE((ret_func && !ret_func->use_empty()));
+    REQUIRE((!jmp_func || jmp_func->use_empty()));
+  }
+}
+
+TEST_SUITE("TransformRemillJump_ARM32") {
+  TEST_CASE("Run the pass on function having _remill_jump as tail call") {
+    llvm::LLVMContext llvm_context;
+    auto module = LoadTestData(llvm_context, "TransformRemillJumpDataARM32.ll");
+
+    auto arch = remill::Arch::Build(&llvm_context, remill::GetOSName("linux"),
+                                    remill::GetArchName("aarch32"));
+    REQUIRE(arch != nullptr);
+
+    anvill::LifterOptions options(arch.get(), *module.get(), nullptr);
+
+    // memory and types will not get used and create lifter with null
+    anvill::EntityLifter lifter(options, nullptr, nullptr);
+
+    CHECK(RunFunctionPass(module.get(),
+                          CreateTransformRemillJumpIntrinsics(lifter)));
+
+    const auto ret_func = module->getFunction("__remill_function_return");
+    const auto jmp_func = module->getFunction("__remill_jump");
+
+
+    REQUIRE((ret_func && !ret_func->use_empty()));
+    REQUIRE((!jmp_func || jmp_func->use_empty()));
   }
 }
 
