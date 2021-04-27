c56a3b83f6a63e4c3fde502e8dd2dde1ed35e498
Akshay K <iit.akshay@gmail.com>
2021-04-26 21:14:42 -0400

    disable memory map for thumb2 functions in spec

--- anvill/python/anvill/binja/bnfunction.py
+++ anvill/python/anvill/binja/bnfunction.py
@@ -41,6 +41,13 @@ class BNFunction(Function):
         if not is_definition:
             return
 
+        # The lifter does not support thumb2 instruction set. If the function
+        # is of arch type `thumb2` then don't visit them and fill the memory
+        # bytes. These functions will be declared but not defined in the lifted
+        # code
+        if self._bn_func.arch.name == "thumb2":
+            return
+
         mem = program.memory
 
         ref_eas: Set[int] = set()
