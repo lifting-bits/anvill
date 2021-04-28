c19a928eeb423befead51f6df53ac5d22e4bee25
Peter Goodman <peter@trailofbits.com>
2021-04-28 12:47:03 -0400

    Also take in a binary via to anvill.get_program (#183)

--- anvill/python/anvill/binja/__init__.py
+++ anvill/python/anvill/binja/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from typing import Optional
+from typing import Optional, Union
 
 
 import binaryninja as bn
@@ -28,22 +28,35 @@ from anvill.program import *
 
 
 def get_program(
-    binary_path: str, maybe_base_address: Optional[int] = None, cache: bool = False
+    binary_path_or_bv: Union[str, bn.BinaryView], maybe_base_address: Optional[int] = None, cache: bool = False
 ) -> Optional[Program]:
     if cache:
         DEBUG("Ignoring deprecated `cache` parameter to anvill.get_program")
 
-    if maybe_base_address is not None:
-        # Force the new image base address; according to the documentation, we will
-        # not inherit any of the default load options that we get when calling the
-        # get_view_of_file method
-        bv = bn.BinaryViewType.get_view_of_file_with_options(
-            binary_path, options={"loader.imageBase": maybe_base_address}
-        )
+    bv: Optional[bv.BinaryView] = None
+    binary_path: str = ""
 
-    else:
-        # Use the auto-generated load options
-        bv = bn.BinaryViewType.get_view_of_file(binary_path)
+    if isinstance(binary_path_or_bv, bn.BinaryView):
+        bv = binary_path_or_bv
+        try:
+            binary_path = bv.file.filename
+        except:
+            pass
+        assert maybe_base_address is None
+
+    elif isinstance(binary_path_or_bv, str):
+        binary_path: str = binary_path_or_bv
+        if maybe_base_address is not None:
+            # Force the new image base address; according to the documentation, we will
+            # not inherit any of the default load options that we get when calling the
+            # get_view_of_file method
+            bv = bn.BinaryViewType.get_view_of_file_with_options(
+                binary_path, options={"loader.imageBase": maybe_base_address}
+            )
+
+        else:
+            # Use the auto-generated load options
+            bv = bn.BinaryViewType.get_view_of_file(binary_path)
 
     if bv is None:
         DEBUG("Failed to create the BinaryView")
