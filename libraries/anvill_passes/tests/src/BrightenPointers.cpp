4cd038d1e390927e14d4a427944642054e81b09c
Carson Harmon <carsonharmon@protonmail.com>
2021-05-04 09:52:26 -0700

    Formatting + investigating regression on test1

--- libraries/anvill_passes/tests/src/BrightenPointers.cpp
+++ libraries/anvill_passes/tests/src/BrightenPointers.cpp
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <anvill/Analysis/CrossReferenceResolver.h>
 #include <anvill/Lifters/EntityLifter.h>
 #include <anvill/Lifters/Options.h>
 #include <anvill/Providers/MemoryProvider.h>
@@ -22,44 +23,45 @@
 #include <anvill/Transforms.h>
 #include <doctest.h>
 #include <llvm/IR/Verifier.h>
-#include <remill/BC/Lifter.h>
-#include <remill/OS/OS.h>
 #include <remill/Arch/Arch.h>
 #include <remill/Arch/Name.h>
-#include <anvill/Analysis/CrossReferenceResolver.h>
+#include <remill/BC/Lifter.h>
+#include <remill/OS/OS.h>
+
 #include <iostream>
 
 #include "Utils.h"
 
 namespace anvill {
 
-void printFunc(const llvm::Module& mod, const std::string& name, const std::string& msg) {
-  for (auto& f : mod) {
+void printFunc(const llvm::Module &mod, const std::string &name,
+               const std::string &msg) {
+  for (auto &f : mod) {
     if (f.hasName() && f.getName() == name) {
       std::cout << msg << std::endl;
       f.print(llvm::errs(), nullptr);
     }
   }
-
 }
-bool checkMod(const llvm::Module& mod) {
-    // Verify the module
-    std::string error_buffer;
-    llvm::raw_string_ostream error_stream(error_buffer);
+bool checkMod(const llvm::Module &mod) {
 
-    auto succeeded = llvm::verifyModule(mod, &error_stream) == 0;
-    error_stream.flush();
+  // Verify the module
+  std::string error_buffer;
+  llvm::raw_string_ostream error_stream(error_buffer);
 
-    CHECK(succeeded);
-    if (!succeeded) {
-      std::string error_message = "Module verification failed";
-      if (!error_buffer.empty()) {
-        error_message += ": " + error_buffer;
-      }
+  auto succeeded = llvm::verifyModule(mod, &error_stream) == 0;
+  error_stream.flush();
 
-      std::cerr << error_message << std::endl;
+  CHECK(succeeded);
+  if (!succeeded) {
+    std::string error_message = "Module verification failed";
+    if (!error_buffer.empty()) {
+      error_message += ": " + error_buffer;
     }
-    return succeeded;
+
+    std::cerr << error_message << std::endl;
+  }
+  return succeeded;
 }
 /*
 class BrightenPointersFixture {
@@ -76,7 +78,7 @@ class BrightenPointersFixture {
   const std::shared_ptr<TypeProvider> types;
 };
 */
-bool RunFunctionPass(llvm::Module& module, llvm::FunctionPass *function_pass) {
+bool RunFunctionPass(llvm::Module &module, llvm::FunctionPass *function_pass) {
   llvm::legacy::FunctionPassManager pass_manager(&module);
   pass_manager.add(function_pass);
 
@@ -91,48 +93,47 @@ bool RunFunctionPass(llvm::Module& module, llvm::FunctionPass *function_pass) {
 }
 
 TEST_SUITE("BrightenPointers") {
+
   TEST_CASE("Run the whole pass on a well-formed function") {
 
     llvm::LLVMContext context;
     auto mod = LoadTestData(context, "gep_add.ll");
-    //printFunc(*mod, "main", "BEFORE");
     REQUIRE(mod != nullptr);
     CHECK(RunFunctionPass(*mod, CreateBrightenPointerOperations(250U)));
     CHECK(checkMod(*mod));
-    //printFunc(*mod, "main", "AFTER");
-
   }
 
   TEST_CASE("multiple_bitcast") {
     llvm::LLVMContext context;
     auto mod = LoadTestData(context, "multiple_bitcast.ll");
-    //printFunc(*mod, "valid_test", "BEFORE");
-    // printFunc(*mod, "memcpy", "BEFORE");
     REQUIRE(mod != nullptr);
     CHECK(RunFunctionPass(*mod, CreateBrightenPointerOperations(250U)));
     CHECK(checkMod(*mod));
-    //printFunc(*mod, "valid_test", "AFTER");
-    // printFunc(*mod, "memcpy", "AFTER");
-}
+  }
 
   TEST_CASE("don't crash on loops") {
     llvm::LLVMContext context;
     auto mod = LoadTestData(context, "loop_test.ll");
-    //printFunc(*mod, "main", "BEFORE");
     REQUIRE(mod != nullptr);
     CHECK(RunFunctionPass(*mod, CreateBrightenPointerOperations(250U)));
     CHECK(checkMod(*mod));
-    //printFunc(*mod, "main", "AFTER");
   }
 
   TEST_CASE("challenge 1") {
     llvm::LLVMContext context;
     auto mod = LoadTestData(context, "rx_message.ll");
-    //printFunc(*mod, "rx_message_routine", "BEFORE");
     REQUIRE(mod != nullptr);
     CHECK(RunFunctionPass(*mod, CreateBrightenPointerOperations(250U)));
     CHECK(checkMod(*mod));
-    //printFunc(*mod, "rx_message_routine", "AFTER");
+  }
+
+
+  TEST_CASE("buggy challenge") {
+    llvm::LLVMContext context;
+    auto mod = LoadTestData(context, "bug.ll");
+    REQUIRE(mod != nullptr);
+    CHECK(RunFunctionPass(*mod, CreateBrightenPointerOperations(250U)));
+    CHECK(checkMod(*mod));
   }
 }
 
