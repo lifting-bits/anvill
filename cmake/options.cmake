49bdc45c88a29a09bd86cfcc71c072a130ad27da
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-05-26 14:47:32 -0400

    cmake: Add option to not build Python components (#206)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,6 +6,8 @@
 # the LICENSE file found in the root directory of this source tree.
 #
 
+include(CMakeDependentOption)
+
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
   set(default_build_type "Release")
 else()
@@ -14,9 +16,11 @@ endif()
 
 set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE STRING "Build type")
 
-option(ANVILL_ENABLE_INSTALL_TARGET "Set to ON to enable the install directives. This installs both the native and python components" true)
-option(ANVILL_ENABLE_TESTS "Set to ON to enable the tests" true)
-option(ANVILL_INSTALL_PYTHON3_LIBS "Install Python 3 libraries to the **local machine** at build time. Mostly used for local development, not required for packaging")
+option(ANVILL_ENABLE_INSTALL_TARGET "Set to ON to enable the install directives. This installs both the native and python components" TRUE)
+option(ANVILL_ENABLE_PYTHON3_LIBS "Build Python 3 libraries" TRUE)
+cmake_dependent_option(ANVILL_INSTALL_PYTHON3_LIBS "Install Python 3 libraries to the **local machine** at build time. Mostly used for local development, not required for packaging" FALSE
+  "ANVILL_ENABLE_PYTHON3_LIBS" FALSE)
+option(ANVILL_ENABLE_TESTS "Set to ON to enable the tests" TRUE)
 option(ANVILL_ENABLE_SANITIZERS "Set to ON to enable sanitizers. May not work with VCPKG")
 
 set(VCPKG_ROOT "" CACHE FILEPATH "Root directory to use for vcpkg-managed dependencies")
