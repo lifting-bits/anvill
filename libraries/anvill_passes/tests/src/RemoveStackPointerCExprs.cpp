2ea532a04a8bf803a7028c4a9ed389397bc8706b
2over12 <ian.smith@trailofbits.com>
2021-11-12 19:05:06 -0500

    added pass to remove stack pointer constantexpressions for stack recovery

---
+++ libraries/anvill_passes/tests/src/RemoveStackPointerCExprs.cpp
@@ -0,0 +1,63 @@
+#include "RemoveStackPointerCExprs.h"
+
+#include <anvill/Transforms.h>
+#include <doctest.h>
+#include <llvm/ADT/SmallSet.h>
+#include <llvm/IR/Dominators.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <remill/Arch/Arch.h>
+#include <remill/Arch/Name.h>
+
+#include <iostream>
+
+#include "Utils.h"
+namespace anvill {
+
+static llvm::Function *FindFunction(llvm::Module *module, std::string name) {
+  for (auto &function : *module) {
+    if (function.getName().equals(name)) {
+      return &function;
+    }
+  }
+  return nullptr;
+}
+
+TEST_SUITE("RemoveStackPointerCExprs") {
+  TEST_CASE("RegressionRecoverStack.ll") {
+    llvm::LLVMContext llvm_context;
+    auto mod = LoadTestData(llvm_context, "RegressionRecoverStack.ll");
+    auto target_function = FindFunction(mod.get(), "slice");
+    CHECK(target_function != nullptr);
+    llvm::FunctionPassManager fpm;
+    llvm::FunctionAnalysisManager fam;
+    llvm::ModuleAnalysisManager mam;
+    llvm::LoopAnalysisManager lam;
+    llvm::CGSCCAnalysisManager cgam;
+
+    llvm::PassBuilder pb;
+
+    pb.registerFunctionAnalyses(fam);
+    pb.registerModuleAnalyses(mam);
+    pb.registerCGSCCAnalyses(cgam);
+    pb.registerLoopAnalyses(lam);
+
+    pb.crossRegisterProxies(lam, fam, cgam, mam);
+
+
+    fpm.addPass(RemoveStackPointerCExprs());
+    fpm.run(*target_function, fam);
+
+    target_function->dump();
+
+    CHECK(VerifyModule(mod.get()));
+
+    fam.clear();
+    cgam.clear();
+    lam.clear();
+    mam.clear();
+  }
+}
+}  // namespace anvill
