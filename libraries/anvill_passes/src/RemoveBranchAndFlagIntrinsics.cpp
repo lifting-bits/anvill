45bf3bb74cbc77d9d233c1c568a2bf4796e8bdbb
2over12 <ian.smith@trailofbits.com>
2021-12-03 12:34:06 -0500

    fixed uaf

--- libraries/anvill_passes/src/RemoveBranchAndFlagIntrinsics.cpp
+++ libraries/anvill_passes/src/RemoveBranchAndFlagIntrinsics.cpp
@@ -5,24 +5,31 @@
 #include <llvm/IR/InstrTypes.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/Support/Casting.h>
+#include <llvm/Transforms/Utils/BasicBlockUtils.h>
+
 namespace anvill {
 
 llvm::PreservedAnalyses
 RemoveBranchAndFlagIntrinsics::run(llvm::Function &F,
                                    llvm::FunctionAnalysisManager &AM) {
 
+  std::vector<llvm::Instruction *> to_del;
   for (auto &insn : llvm::instructions(F)) {
     if (auto *call = llvm::dyn_cast<llvm::CallBase>(&insn)) {
       auto name = call->getCalledFunction()->getName();
       if (name.startswith(kFlagIntrinsicPrefix) ||
           name.startswith(kCompareInstrinsicPrefix)) {
 
+
         call->replaceAllUsesWith(call->getArgOperand(0));
-        call->eraseFromParent();
+        to_del.push_back(call);
       }
     }
   }
 
+  for (auto c : to_del) {
+    c->eraseFromParent();
+  }
 
   return llvm::PreservedAnalyses::none();
 }
