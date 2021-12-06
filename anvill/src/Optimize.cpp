5143fc725a15324d286f9a09c2b68a95e8d31899
2over12 <ian.smith@trailofbits.com>
2021-12-06 08:20:19 -0500

    removed old remove pass

--- anvill/src/Optimize.cpp
+++ anvill/src/Optimize.cpp
@@ -151,7 +151,6 @@ void OptimizeModule(const EntityLifter &lifter_context,
   auto error_manager_ptr = ITransformationErrorManager::Create();
   auto &err_man = *error_manager_ptr.get();
 
-  AddRemoveComparisonAndBranchIntrinsics(fpm);
   AddSinkSelectionsIntoBranchTargets(fpm, err_man);
   AddRemoveUnusedFPClassificationCalls(fpm);
   AddRemoveDelaySlotIntrinsics(fpm);
@@ -175,7 +174,6 @@ void OptimizeModule(const EntityLifter &lifter_context,
   AddSplitStackFrameAtReturnAddress(fpm, err_man);
   fpm.addPass(llvm::SROA());
   AddBranchRecovery(fpm);
-
 
 
   // Sometimes we have a values in the form of (expr ^ 1) used as branch
