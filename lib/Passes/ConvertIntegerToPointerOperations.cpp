6861973ea514176a71f5f84b97897cd2377dad2b
2over12 <ian.smith@trailofbits.com>
2022-01-10 13:31:15 -0500

    many pointer lift fixes

--- lib/Passes/ConvertIntegerToPointerOperations.cpp
+++ lib/Passes/ConvertIntegerToPointerOperations.cpp
@@ -10,14 +10,15 @@
 #include <anvill/Transforms.h>
 #include <llvm/IR/PatternMatch.h>
 
+#include <vector>
+
 #include "Utils.h"
 
 namespace anvill {
 
 llvm::StringRef ConvertIntegerToPointerOperations::name(void) {
-  return "PointerLifter";
+  return "ConvertIntegerToPointerOperations";
 }
-
 
 namespace pats = llvm::PatternMatch;
 
@@ -25,46 +26,69 @@ llvm::PreservedAnalyses
 ConvertIntegerToPointerOperations::run(llvm::Function &func,
                                        llvm::FunctionAnalysisManager &fam) {
 
-  llvm::Value *index = nullptr;
-  llvm::Value *base = nullptr;
-  bool changed = false;
+  auto &dl = func.getParent()->getDataLayout();
+
+  struct Match {
+    llvm::Value *base;
+    llvm::PointerType *ptr_ty;  // Needed for address space.
+    llvm::Type *elem_ty;
+    llvm::Value *index;
+    llvm::IntToPtrInst *itp;
+  };
+
+  std::vector<Match> matches;
+
   for (auto &insn : llvm::instructions(func)) {
+    Match match;
+    match.itp = llvm::dyn_cast<llvm::IntToPtrInst>(&insn);
+    if (match.itp) {
+      match.ptr_ty = llvm::dyn_cast<llvm::PointerType>(match.itp->getType());
+      match.elem_ty = match.ptr_ty->getElementType();
 
 
-    if (auto ptr_ty = llvm::dyn_cast<llvm::PointerType>(insn.getType())) {
-      auto elem_ty = ptr_ty->getElementType();
-      auto elem_size_bits = elem_ty->getScalarSizeInBits();
+      if (!match.elem_ty->isSized()) {
+        continue;
+      }
+
+
+      auto elem_size = dl.getTypeSizeInBits(match.elem_ty);
+      if (elem_size.isScalable()) {
+        continue;
+      }
+
       uint64_t shifting_left_by = 0;
-      if (elem_ty->isIntegerTy()) {
-        llvm::Instruction *insn_ptr = &insn;
-        if (pats::match(insn_ptr,
-                        pats::m_IntToPtr(pats::m_Add(
-                            pats::m_Shl(pats::m_Value(index),
-                                        pats::m_ConstantInt(shifting_left_by)),
-                            pats::m_Value(base))))) {
-          uint64_t byte_size = 1 << shifting_left_by;
-          uint64_t shl_bit_isze = byte_size * 8;
+      uint64_t elem_size_bits = elem_size.getFixedSize();
 
-          if (shl_bit_isze == elem_size_bits) {
-            auto base_ptr = new llvm::IntToPtrInst(base, ptr_ty);
-            std::vector<llvm::Value *> idxes = {index};
-            ptr_ty->dump();
-            base_ptr->dump();
-            index->dump();
-            auto gep = llvm::GetElementPtrInst::Create(ptr_ty, base_ptr, idxes);
-            insn_ptr->replaceAllUsesWith(gep);
-            changed = true;
-          }
-        }
+      if (!pats::match(match.itp,
+                       pats::m_IntToPtr(pats::m_Add(
+                           pats::m_Shl(pats::m_Value(match.index),
+                                       pats::m_ConstantInt(shifting_left_by)),
+                           pats::m_Value(match.base))))) {
+        continue;
+      }
+
+      uint64_t byte_size = 1ull << shifting_left_by;
+      uint64_t shl_bit_size = byte_size * 8;
+
+      if (shl_bit_size == elem_size_bits) {
+        matches.emplace_back(std::move(match));
       }
     }
   }
 
-  if (changed) {
-    return llvm::PreservedAnalyses::none();
-  } else {
+  if (matches.empty()) {
     return llvm::PreservedAnalyses::all();
   }
+
+  for (auto [base, ptr_ty, elem_ty, index, itp] : matches) {
+    auto base_ptr = new llvm::IntToPtrInst(base, ptr_ty, "", itp);
+    llvm::Value *idxes[] = {index};
+    auto gep =
+        llvm::GetElementPtrInst::Create(elem_ty, base_ptr, idxes, "", itp);
+    itp->replaceAllUsesWith(gep);
+  }
+
+  return llvm::PreservedAnalyses::none();
 }
 
 void AddConvertIntegerToPointerOperations(llvm::FunctionPassManager &fpm) {
