40fbf0d4d492a76695ed707189742ff25bad1c2f
Artem Dinaburg <artem@trailofbits.com>
2022-01-11 12:32:29 -0800

    Update gitignore and build script

--- scripts/build-preset.sh
+++ scripts/build-preset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT=remill
+PROJECT=anvill
 
 BUILDLOG=${PROJECT}-build.log
 CONFIGLOG=${PROJECT}-configure.log
