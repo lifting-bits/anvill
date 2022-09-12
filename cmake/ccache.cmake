6aadc224a998857ff7edf24c100d18f718f13404
William Tan <Ninja3047@users.noreply.github.com>
2022-09-12 11:47:04 -0400

    fix typos in ccache.cmake

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -8,13 +8,13 @@
 
 if(PLATFORM_LINUX OR PLATFORM_MACOS)
   find_program(ccache_executable "ccache")
-  if(NOT ccache_executable STREQUAL "ccache_executable-NOFOUND")
-    message(STATUS "multiplier: Enabling ccache support (${ccache_executable})")
+  if(NOT ccache_executable STREQUAL "ccache_executable-NOTFOUND")
+    message(STATUS "${PROJECT_NAME}: Enabling ccache support (${ccache_executable})")
 
     set(CMAKE_CXX_COMPILER_LAUNCHER "${ccache_executable}" CACHE FILEPATH "ccache")
     set(CMAKE_C_COMPILER_LAUNCHER "${ccache_executable}" CACHE FILEPATH "ccache")
 
   else()
-    message(STATUS "multiplier: No ccache executable found")
+    message(STATUS "${PROJECT_NAME}: No ccache executable found")
   endif()
 endif()
