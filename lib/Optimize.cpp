ca29ee667f04571bb6a12d7dbcc0aaa3eb05bb87
2over12 <ian.smith@trailofbits.com>
2023-01-12 11:32:43 -0500

    Revert "Don't run the first set of function passes again"
    This reverts commit 0648c6fb644d1db5882c0a032e0e2009d1d2d7e1.

--- lib/Optimize.cpp
+++ lib/Optimize.cpp
@@ -287,7 +287,6 @@ void OptimizeModule(const EntityLifter &lifter, llvm::Module &module,
   mpm.addPass(llvm::createModuleToFunctionPassAdaptor(std::move(fpm)));
   mpm.run(module, mam);
 
-  llvm::ModulePassManager second_mpm;
   llvm::FunctionPassManager second_fpm;
 
   AddTransformRemillJumpIntrinsics(second_fpm, xr);
@@ -316,9 +315,8 @@ void OptimizeModule(const EntityLifter &lifter, llvm::Module &module,
   second_fpm.addPass(llvm::VerifierPass());
 
 
-  second_mpm.addPass(
-      llvm::createModuleToFunctionPassAdaptor(std::move(second_fpm)));
-  second_mpm.run(module, mam);
+  mpm.addPass(llvm::createModuleToFunctionPassAdaptor(std::move(second_fpm)));
+  mpm.run(module, mam);
 
   // Get rid of all final uses of `__anvill_pc`.
   if (lifter.Options().should_remove_anvill_pc) {
