ead7b158b0cf20aff3cb50b087946cdc380beb7d
William Tan <1284324+Ninja3047@users.noreply.github.com>
2023-04-04 18:25:58 -0400

    python script to print diff between llvm opts

---
+++ scripts/diff-opts.py
@@ -0,0 +1,57 @@
+import argparse
+import tempfile
+import re
+import subprocess
+
+def run_diff(name, before, after):
+    if info_match := re.search("define [^ ]+ @func([0-9]+)basic_block([0-9]+)", before[1]):
+        func_addr = int(info_match[1])
+        bb_addr = int(info_match[2])
+        print(f"Function address: {func_addr:x} BB addrs: {bb_addr:x}")
+
+    before = '\n'.join(b.rstrip() for b in before).strip()
+    after = '\n'.join(a.rstrip() for a in after).strip()
+    with tempfile.NamedTemporaryFile() as b, tempfile.NamedTemporaryFile() as a:
+        b.write(before.encode())
+        a.write(after.encode())
+        b.flush()
+        a.flush()
+        result = subprocess.run(['icdiff', '-W', b.name, a.name], stdout=subprocess.PIPE)
+        print(f"Step: {name}")
+        if result.stdout:
+            print(result.stdout.decode())
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", type=argparse.FileType("r"))
+    args = parser.parse_args()
+
+    with args.file as f:
+        result = f.read()
+    lines = result.split('\n')
+    name = None
+    before = []
+    after = []
+    is_before = None
+    for line in lines:
+        if line.startswith('*** IR Dump After'):
+            is_before = False
+        elif line.startswith('*** IR Dump Before'):
+            name_match = re.search("\\(([^\\)]+)\\)", line)
+            if name_match:
+                name = name_match.group(1)
+            if len(before) > 0 and len(after) > 0:
+                run_diff(name, before, after)
+                before = []
+                after = []
+            is_before = True
+        else:
+            if is_before:
+                before.append(line)
+            else:
+                after.append(line)
+
+    run_diff(name, before, after)
+
+if __name__ == '__main__':
+    main()
